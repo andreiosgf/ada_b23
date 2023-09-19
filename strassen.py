0bde31e54868cf1b07a4edb1184c0c639c433842
Andrés G. Floriano <andreiosgf@gmail.com>
2023-09-18 21:05:57 -0600

    strassen add
    Adding strassen.py code

---
+++ strassen.py
@@ -0,0 +1,66 @@
+# Version 3.6
+
+import numpy as np
+import time as time
+def split(matrix):
+	"""
+	Splits a given matrix into quarters.
+	Input: nxn matrix
+	Output: tuple containing 4 n/2 x n/2 matrices corresponding to a, b, c, d
+	"""
+	row, col = matrix.shape
+	row2, col2 = row//2, col//2
+	return matrix[:row2, :col2], matrix[:row2, col2:], matrix[row2:, :col2], matrix[row2:, col2:]
+
+def strassen(x, y):
+	"""
+	Computes matrix product by divide and conquer approach, recursively.
+	Input: nxn matrices x and y
+	Output: nxn matrix, product of x and y
+	"""
+
+	# Base case when size of matrices is 1x1
+	if len(x) == 1:
+		return x * y
+
+	# Splitting the matrices into quadrants. This will be done recursively
+	# until the base case is reached.
+	a, b, c, d = split(x)
+	e, f, g, h = split(y)
+
+	# Computing the 7 products, recursively (p1, p2...p7)
+	p1 = strassen(a, f - h)
+	p2 = strassen(a + b, h)
+	p3 = strassen(c + d, e)
+	p4 = strassen(d, g - e)
+	p5 = strassen(a + d, e + h)
+	p6 = strassen(b - d, g + h)
+	p7 = strassen(a - c, e + f)
+
+	# Computing the values of the 4 quadrants of the final matrix c
+	c11 = p5 + p4 - p2 + p6
+	c12 = p1 + p2
+	c21 = p3 + p4
+	c22 = p1 + p5 - p3 - p7
+
+	# Combining the 4 quadrants into a single matrix by stacking horizontally and vertically.
+	c = np.vstack((np.hstack((c11, c12)), np.hstack((c21, c22))))
+
+	return c
+
+if __name__=='__main__':
+	'''
+	This works only with powers of 2
+	'''
+
+	A = np.random.randint(-100, 100, (512, 512))
+	#print(f'A={A}')
+	B = np.random.randint(-100, 100, (512, 512))
+	#print(f'B={B}')
+	n = len(A)
+	C = np.zeros((n, n))
+	start=time.time()
+	C=strassen(A, B)
+	end=time.time()
+	print(f'C={C}')
+	print(f'Elapsed time {end-start}')
